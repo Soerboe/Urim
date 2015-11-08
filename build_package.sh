5648f8c8bccf84182d68a7061c97f04ebc773a83
Dag Henning Liodden Sørbø <daghenning@lioddensorbo.com>
2015-11-08 20:14:15 +0100

    Adds very simple package script

---
+++ build_package.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+
+project_folder=( `pwd` )
+build_folder=${project_folder}/../build-UrimThummim-Release
+dll_folder=${project_folder}/../urim-dlls
+app_name=UrimThummim
+
+#echo $project_folder
+#echo $build_folder
+#echo $dll_folder
+
+echo "Version:"
+read version
+
+if [ "${version}" == "" ]; then
+	echo "No version given"
+	exit 1
+fi
+
+package_folder=${app_name}_v${version}
+cd ${build_folder}
+mkdir ${package_folder}
+cp ./app/release/${app_name}.exe ${package_folder}
+cp ./core/release/core.dll ${package_folder}
+cp ./gui/release/gui.dll ${package_folder}
+cp ${dll_folder}/* ${package_folder}/
+
+windeployqt ${package_folder}/${app_name}.exe
+
+echo ""
+echo "Done"

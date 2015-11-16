f16c6ccd4b2bd0fe49580e0d2215faab4ed00497
Dag Henning Liodden Sørbø <daghenning@lioddensorbo.com>
2015-11-16 18:17:04 +0100

    Bump version to 0.3.0
    Change of name requires a new minor

--- app/app.h
+++ app/app.h
@@ -20,7 +20,7 @@
 #define APPLICATION_NAME "Urim"
 #define ORG_NAME "Dag Henning Liodden Sorbo"
 #define ORG_DOMAIN "lioddensorbo.com"
-#define APPLICATION_VERSION "0.2.0"
+#define APPLICATION_VERSION "0.3.0"
 
 #endif // APP_H
 

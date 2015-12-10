22e0793b971ea395f4cf191e39e12f452efab9ab
Dag Henning Liodden Sørbø <daghenning@lioddensorbo.com>
2015-12-10 10:37:06 +0100

    Adds download page link to updates xml

--- website/wp-theme/urim/updates.php
+++ website/wp-theme/urim/updates.php
@@ -9,11 +9,15 @@ Template Name: Urim updates
   $postid = $wp_query->post->ID;
   $latestVersion = get_post_meta($postid, 'urim-latest-version', true);
 
+  $downloadPage = get_page_by_title('Downloads');
+  $downloadPageUrl = get_page_link($downloadPage->ID);
+
   $xml =
 '<?xml version="1.0" encoding="utf-8"?>
 <app>
     <name>Urim</name>
-    <latest-version>'.$latestVersion.'</latest-version>
+    <latest-version>' . $latestVersion . '</latest-version>
+    <download-page-url>' . $downloadPageUrl . '</download-page-url>
 </app>';
   print($xml);
 ?>

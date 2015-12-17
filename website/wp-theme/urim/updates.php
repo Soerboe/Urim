ea7fcba3cc1ce6ca55c321ffc6b2fe1229962eda
Dag Henning Liodden Sørbø <daghenning@lioddensorbo.com>
2015-12-17 13:16:19 +0100

    Adds version check in updates.php

--- website/wp-theme/urim/updates.php
+++ website/wp-theme/urim/updates.php
@@ -9,27 +9,35 @@ Template Name: Urim updates
     $lang = 'en'; // english default language
   }
 
+  $currentVersion = $_GET['v'];
+
   global $wp_query;
   $postid = $wp_query->post->ID;
   $latestVersion = get_post_meta($postid, 'urim-latest-version', true);
   $downloadPageTitle = get_post_meta($postid, 'download-page-' . $lang, true);
 
-  if (!$latestVersion || !$downloadPageTitle) {
+  if (!$latestVersion || !$downloadPageTitle || empty($currentVersion)) {
     http_response_code(500);
     exit(-1);
   }
 
+  header('Content-Type: application/xml');
+  $xml =
+  '<?xml version="1.0" encoding="utf-8"?>
+    <application-info version="1.0">
+      <name>Urim</name>';
+
+  $xml .= '<has-update>';
+  $xml .= (version_compare($currentVersion, $latestVersion) <= 0) ? 'false' : 'true';
+  $xml .= '</has-update>';
+
   $downloadPage = get_page_by_title($downloadPageTitle);
   $downloadPageUrl = get_page_link($downloadPage->ID);
 
-  $xml =
-'<?xml version="1.0" encoding="utf-8"?>
-<app>
-    <name>Urim</name>
-    <latest-version>' . $latestVersion . '</latest-version>
-    <download-page-url>' . $downloadPageUrl . '</download-page-url>
-</app>';
+  $xml .=
+      '<latest-version>' . $latestVersion . '</latest-version>
+      <download-page-url>' . $downloadPageUrl . '</download-page-url>
+    </application-info>';
 
-  header('Content-Type: application/xml');
   print($xml);
 ?>
